026706c9ebb7bc573939bcaf189011a234ab0c81
andresgutivar <141797757+andresgutivar@users.noreply.github.com>
2024-09-17 20:53:22 -0300

    update

---
+++ lib/loginScreen.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      body: const TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          border: OutlineInputBorder(),
+          labelText: 'Password',
+        ),
+      ),
+    );
+  }
+}
