417f95bbe5b402c87ded99c24ca71f563c6f9da0
andresgutivar <141797757+andresgutivar@users.noreply.github.com>
2024-09-19 20:21:52 -0300

    pruebas

--- lib/pages/home/homePage.dart
+++ lib/pages/home/homePage.dart
@@ -6,46 +6,88 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        actions: <Widget>[
-          // Agregar un widget Row para combinar botones con íconos y texto
-          Row(
-            children: [
-              TextButton.icon(
-                icon: const Icon(Icons.add_alert,
-                    color: Color.fromARGB(255, 0, 0, 0)),
-                label: const Text(
-                  'Alert',
-                  style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+        appBar: AppBar(
+          actions: <Widget>[
+            // Agregar un widget Row para combinar botones con íconos y texto
+            Row(
+              children: [
+                const CircleAvatar(
+                  backgroundColor: Color.fromARGB(255, 50, 246, 0),
+                  child: Text('AH'),
                 ),
-                onPressed: () {
-                  ScaffoldMessenger.of(context).showSnackBar(
-                      const SnackBar(content: Text('This is a snackbar')));
-                },
-              ),
-              const SizedBox(
-                  width: 16), // Espacio entre el primer y segundo botón
-              TextButton.icon(
-                icon: const Icon(Icons.navigate_next,
-                    color: Color.fromARGB(255, 0, 0, 0)),
-                label: const Text(
-                  'Next',
-                  style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                TextButton.icon(
+                  icon: const Icon(Icons.add_alert,
+                      color: Color.fromARGB(255, 0, 0, 0)),
+                  label: const Text(
+                    'Alert',
+                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  onPressed: () {
+                    ScaffoldMessenger.of(context).showSnackBar(
+                        const SnackBar(content: Text('This is a snackbar')));
+                  },
                 ),
-                onPressed: () {
-                  Navigator.pushNamed(context, '/');
-                },
-              ),
-            ],
-          ),
-        ],
-      ),
-      body: const Center(
-        child: Text(
-          'This is the home page',
-          style: TextStyle(fontSize: 24),
+                const SizedBox(
+                    width: 16), // Espacio entre el primer y segundo botón
+                TextButton.icon(
+                  icon: const Icon(Icons.navigate_next,
+                      color: Color.fromARGB(255, 0, 0, 0)),
+                  label: const Text(
+                    'Next',
+                    style: TextStyle(color: Color.fromARGB(255, 0, 0, 0)),
+                  ),
+                  onPressed: () {
+                    Navigator.pushNamed(context, '/');
+                  },
+                ),
+              ],
+            ),
+          ],
         ),
-      ),
-    );
+        body: // Generated code for this Column Widget...
+            Column(
+          mainAxisSize: MainAxisSize.max,
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Card(
+              clipBehavior: Clip.antiAliasWithSaveLayer,
+              elevation: 0,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Row(
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Container(
+                    width: 200,
+                    height: 200,
+                    clipBehavior: Clip.antiAlias,
+                    decoration: const BoxDecoration(
+                      shape: BoxShape.circle,
+                    ),
+                    child: Image.network(
+                      'https://picsum.photos/seed/470/600',
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                  const Column(
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Hello World',
+                      ),
+                      Text(
+                        'Hello World',
+                      ),
+                      ElevatedButton(
+                          onPressed: AlertDialog.new, child: Text("data"))
+                    ],
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ));
   }
 }
