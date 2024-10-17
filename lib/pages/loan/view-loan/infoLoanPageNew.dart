6196b03a22574288b6c2de43dd416aa9b84feac7
andresgutivar <andresg.vargasmv@gmail.com>
2024-10-17 09:16:01 -0300

    continuar en pc grande

--- lib/pages/loan/view-loan/infoLoanPageNew.dart
+++ lib/pages/loan/view-loan/infoLoanPageNew.dart
@@ -5,11 +5,21 @@ import 'package:biblioteca/pages/user-book/view_users/view_user_detail_page.dart
 import 'package:biblioteca/pages/user-book/view_users/view_user_detail_page_arguments.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_modal_dialog/flutter_modal_dialog.dart';
 
-class infoLoanPageNew extends StatelessWidget {
+class infoLoanPageNew extends StatefulWidget {
   const infoLoanPageNew({super.key});
   static const routeName = '/loanInfo';
 
+  @override
+  State<infoLoanPageNew> createState() => _infoLoanPageNewState();
+}
+
+class _infoLoanPageNewState extends State<infoLoanPageNew> {
+  final TextEditingController _name = TextEditingController();
+  final TextEditingController _lastName = TextEditingController();
+  final TextEditingController _dni = TextEditingController();
+  final TextEditingController _phone = TextEditingController();
   @override
   Widget build(BuildContext context) {
     const Color customColor = Color(0xfff8FFF7C);
@@ -18,14 +28,20 @@ class infoLoanPageNew extends StatelessWidget {
     print(args.lenderId);
     print(args.userId);
 
-    final Stream<DocumentSnapshot> userStream = FirebaseFirestore.instance
+    FirebaseFirestore.instance
         .collection(UserModel.tableName)
+        .doc(args.lenderId)
         .withConverter(
           fromFirestore: UserModel.fromFirestore,
           toFirestore: (UserModel user, options) => user.toFirestore(),
         )
-        .doc(args.lenderId)
-        .snapshots();
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.exists) {
+        UserModel user = documentSnapshot.data()! as UserModel;
+        _name.text = user.name!;
+      }
+    });
 
     return Scaffold(
       appBar: AppBar(
@@ -40,156 +56,115 @@ class infoLoanPageNew extends StatelessWidget {
               mainAxisAlignment: MainAxisAlignment.center,
               crossAxisAlignment: CrossAxisAlignment.center,
               children: [
-                StreamBuilder<DocumentSnapshot>(
-                  stream: userStream,
-                  builder: (BuildContext context,
-                      AsyncSnapshot<DocumentSnapshot> snapshot) {
-                    // if (snapshot.connectionState == ConnectionState.waiting) {
-                    //   return CircularProgressIndicator();
-                    // }
-                    if (snapshot.hasError) {
-                      return Text('Error: ${snapshot.error}');
-                    } else if (!snapshot.hasData &&
-                        snapshot.connectionState != ConnectionState.waiting) {
-                      return Text(
-                          'Error al cargar los datos. Es posible que el usuario se halla eliminado. Contactese con los encargados de la aplicacion.');
-                    } else {
-                      UserModel user = snapshot.data!.data() as UserModel;
-
-                      return Column(children: [
-                        Text(
-                          'Informacion sobre la prestadora',
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          width: 800,
-                          child: Container(
-                            height: 2,
-                            color: Colors.black,
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        MyText(
-                            text: 'Nombre',
-                            value: user.name!,
-                            customColor: customColor),
-                        SizedBox(height: 16),
-                        MyText(
-                            text: 'Apellido',
-                            value: user.lastName!,
-                            customColor: customColor),
-                        SizedBox(height: 16),
-                        MyText(
-                            text: 'DNI',
-                            value: user.dni!,
-                            customColor: customColor),
-                        SizedBox(height: 16),
-                        MyText(
-                            text: 'Teléfono',
-                            value: user.phone!,
-                            customColor: customColor),
-                        // Otra sección
-                        Text(
-                          'Informacion sobre lector del libro',
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          width: 800,
-                          child: Container(
-                            height: 2,
-                            color: Colors.black,
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        SizedBox(
-                          width: 400,
-                          height: 60,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.black,
-                              side: BorderSide(
-                                color: customColor, // Color del borde
-                                width: 3, // Ancho del borde
-                              ),
-                            ),
-                            onPressed: () => Navigator.pushNamed(
-                              context,
-                              ViewUserDetailPage.routeName,
-                              arguments:
-                                  ViewUserDetailPageArguments(args.userId!),
-                            ),
-                            child: const Text(
-                              'Informacion sobre usuario',
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                        const SizedBox(height: 64),
-                        Text(
-                          'Marcar libro como devuelto',
-                          style: TextStyle(
-                              fontSize: 24, fontWeight: FontWeight.bold),
-                          textAlign: TextAlign.center,
-                        ),
-                        SizedBox(
-                          width: 800,
-                          child: Container(
-                            height: 2,
-                            color: Colors.black,
-                          ),
-                        ),
-                        const SizedBox(height: 16),
-                        SizedBox(
-                          width: 400,
-                          height: 60,
-                          child: ElevatedButton(
-                            style: ElevatedButton.styleFrom(
-                              foregroundColor: Colors.black,
-                              side: BorderSide(
-                                color: customColor, // Color del borde
-                                width: 3, // Ancho del borde
-                              ),
-                            ),
-                            onPressed: () => showDialog<String>(
-                              context: context,
-                              barrierDismissible: false,
-                              useRootNavigator: false,
-                              builder: (BuildContext context) => AlertDialog(
-                                title: const Text('AlertDialog Title'),
-                                content: const Text('AlertDialog description'),
-                                actions: <Widget>[
-                                  TextButton(
-                                    onPressed: () =>
-                                        Navigator.pop(context, 'Cancel'),
-                                    child: const Text('Cancel'),
-                                  ),
-                                  TextButton(
-                                    onPressed: () =>
-                                        Navigator.pop(context, 'OK'),
-                                    child: const Text('OK'),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            child: const Text(
-                              'Devolver',
-                              style: TextStyle(
-                                  fontSize: 18, fontWeight: FontWeight.bold),
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                        ),
-                      ]);
-                    }
-                  },
+                Text(
+                  'Informacion sobre la prestadora',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
                 ),
+                SizedBox(
+                  width: 800,
+                  child: Container(
+                    height: 2,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                MyText(
+                    text: 'Nombre',
+                    value: _name.text,
+                    customColor: customColor),
                 SizedBox(height: 16),
+                MyText(
+                    text: 'Apellido',
+                    value: "user.lastName!",
+                    customColor: customColor),
+                SizedBox(height: 16),
+                MyText(
+                    text: 'DNI', value: "user.dni!", customColor: customColor),
+                SizedBox(height: 16),
+                MyText(
+                    text: 'Teléfono',
+                    value: "user.phone!",
+                    customColor: customColor),
+                // Otra sección
+                Text(
+                  'Informacion sobre lector del libro',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  width: 800,
+                  child: Container(
+                    height: 2,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: 400,
+                  height: 60,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      side: BorderSide(
+                        color: customColor, // Color del borde
+                        width: 3, // Ancho del borde
+                      ),
+                    ),
+                    onPressed: () => Navigator.pushNamed(
+                      context,
+                      ViewUserDetailPage.routeName,
+                      arguments: ViewUserDetailPageArguments(args.userId!),
+                    ),
+                    child: const Text(
+                      'Informacion sobre usuario',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 64),
+                Text(
+                  'Marcar libro como devuelto',
+                  style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  width: 800,
+                  child: Container(
+                    height: 2,
+                    color: Colors.black,
+                  ),
+                ),
+                const SizedBox(height: 16),
+                SizedBox(
+                  width: 400,
+                  height: 60,
+                  child: ElevatedButton(
+                    style: ElevatedButton.styleFrom(
+                      foregroundColor: Colors.black,
+                      side: BorderSide(
+                        color: customColor, // Color del borde
+                        width: 3, // Ancho del borde
+                      ),
+                    ),
+                    onPressed: () {
+                      ModalDialog.confirmation(
+                        context: context,
+                        title: const ModalTitle(text: "Please confirm"),
+                        message: "It's a simple Yes/No question",
+                        confirmButton: const ModalButton(text: "true"),
+                      );
+                    },
+                    child: const Text(
+                      'Devolver',
+                      style:
+                          TextStyle(fontSize: 18, fontWeight: FontWeight.bold),
+                      textAlign: TextAlign.center,
+                    ),
+                  ),
+                ),
               ],
             ),
           ),
@@ -231,33 +206,5 @@ class MyText extends StatelessWidget {
             ),
           ),
         ));
-  }
-}
-
-class DialogExample extends StatelessWidget {
-  const DialogExample({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return TextButton(
-      onPressed: () => showDialog<String>(
-        context: context,
-        builder: (BuildContext context) => AlertDialog(
-          title: const Text('AlertDialog Title'),
-          content: const Text('AlertDialog description'),
-          actions: <Widget>[
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'Cancel'),
-              child: const Text('Cancel'),
-            ),
-            TextButton(
-              onPressed: () => Navigator.pop(context, 'OK'),
-              child: const Text('OK'),
-            ),
-          ],
-        ),
-      ),
-      child: const Text('Show Dialog'),
-    );
   }
 }
