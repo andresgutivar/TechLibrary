f489a35f0975c28ce376185e00b0280ea1c6d716
andresgutivar <141797757+andresgutivar@users.noreply.github.com>
2024-11-07 12:24:04 -0300

    create user finished
    terminado

--- lib/pages/user-book/new_users/new_user_page.dart
+++ lib/pages/user-book/new_users/new_user_page.dart
@@ -3,6 +3,7 @@ import 'package:biblioteca/pages/user-book/new_users/new_user_page_arguments.dar
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
+import 'package:select_searchable_list/select_searchable_list.dart';
 
 class NewUserPage extends StatefulWidget {
   static const routeName = '/newUser';
@@ -23,88 +24,113 @@ class _NewUserPageState extends State<NewUserPage> {
   final TextEditingController _dni = TextEditingController();
   final TextEditingController _phone = TextEditingController();
   final TextEditingController _email = TextEditingController();
-  final TextEditingController _year = TextEditingController();
+  final TextEditingController _year =
+      TextEditingController(text: "Seleccionar anio");
   final TextEditingController _div = TextEditingController();
   final TextEditingController _career = TextEditingController();
+  final List<int> _selectedYear = [1];
+  final Map<int, String> _listYears = {
+    1: 'Seleccionar anio',
+    2: '1',
+    3: '2',
+    4: '3',
+    5: '4',
+    6: '5',
+    7: '6',
+  };
+  final List<int> _selectedCareer = [1];
+
+  final Map<int, String> _listCareer = {
+    1: 'Seleccionar especialidad',
+    2: 'Computacion',
+    3: 'Construccion',
+    4: 'Electronica',
+    5: 'Electricidad',
+    6: 'Mecanica',
+    7: 'Quimica',
+  };
 
   @override
   Widget build(BuildContext context) {
     final args =
         ModalRoute.of(context)!.settings.arguments as NewUserPageArguments;
 
-    void saveToFireBase(user) {
+    void saveToFireBase(UserBookModel user) {
+      // Mostrar el indicador de progreso
+      showDialog(
+        context: context,
+        barrierDismissible:
+            false, // Evita cerrar el diálogo tocando fuera de él
+        builder: (BuildContext context) {
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
+      );
+
       FirebaseFirestore.instance
           .collection(UserBookModel.tableName)
           .doc(user.dni)
           .get()
           .then((documentSnapshot) {
         if (documentSnapshot.exists) {
-          // Si el documento ya existe, mostramos un mensaje de error.
           if (context.mounted) {
             ScaffoldMessenger.of(context).showSnackBar(
               const SnackBar(
                 content: Text('Ya existe un usuario con este DNI.'),
               ),
             );
+            Navigator.of(context).pop(); // Cerrar el diálogo
           }
         } else {
           FirebaseFirestore.instance
               .collection(UserBookModel.tableName)
-              .doc(_dni.text)
-              .withConverter(
-                fromFirestore: UserBookModel.fromFirestore,
-                toFirestore: (UserBookModel user, options) =>
-                    user.toFirestore(),
-              )
-              .set(user)
+              .doc(user.dni)
+              .set(user.toFirestore())
               .then((_) {
             if (context.mounted) {
-              Navigator.of(context).pop();
+              Navigator.of(context).pop(); // Cerrar el diálogo
+              Navigator.of(context).pop(); // Volver a la página anterior
             }
           }).catchError((err) {
-            print(err);
+            debugPrint("Error al guardar usuario: $err");
             if (context.mounted) {
               ScaffoldMessenger.of(context).showSnackBar(
                 const SnackBar(
                   content: Text('Error al guardar los datos del usuario.'),
                 ),
               );
+              Navigator.of(context).pop(); // Cerrar el diálogo
             }
           });
         }
       }).catchError((err) {
-        print(err);
+        debugPrint("Error al verificar existencia del usuario: $err");
         if (context.mounted) {
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(
               content: Text('Error al verificar la existencia del usuario.'),
             ),
           );
+          Navigator.of(context).pop(); // Cerrar el diálogo
         }
       });
     }
 
     void saveUser() {
-      // UserBookModel user = UserBookModel(
-      //   name: _name.text,
-      //   lastName: _lastName.text,
-      //   dni: _dni.text,
-      //   phone: _phone.text,
-      // );
+      int yearValue = 0;
+      if (_year.text.isNotEmpty) {
+        yearValue = int.tryParse(_year.text) ?? 0;
+      }
 
       if (args.userType == UserType.alumno) {
-        int yearValue = 0;
-        if (_year.text.isNotEmpty) {
-          yearValue = int.parse(_year.text);
-        }
-
         if (_name.text.isEmpty ||
             _lastName.text.isEmpty ||
             _dni.text.isEmpty ||
-            _year.text.isEmpty ||
+            _year.text == _listYears[1] ||
             _div.text.isEmpty ||
             _email.text.isEmpty ||
-            (yearValue >= 3 && _career.text.isEmpty)) {
+            (yearValue >= 3 && _career.text == _listCareer[1])) {
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(
               content:
@@ -115,12 +141,12 @@ class _NewUserPageState extends State<NewUserPage> {
         } else if (yearValue > 6) {
           ScaffoldMessenger.of(context).showSnackBar(
             const SnackBar(
-              content: Text('Por favor, ingrese un valor valido para el anio.'),
+              content: Text('Por favor, ingrese un valor válido para el año.'),
             ),
           );
           return;
         } else {
-          UserBookModel user = UserBookModel(
+          final user = UserBookModel(
             rol: "estudiante",
             name: _name.text,
             lastName: _lastName.text,
@@ -134,8 +160,6 @@ class _NewUserPageState extends State<NewUserPage> {
           saveToFireBase(user);
         }
       } else {
-        //logica para el profesor
-
         if (_name.text.isEmpty ||
             _lastName.text.isEmpty ||
             _dni.text.isEmpty ||
@@ -148,7 +172,7 @@ class _NewUserPageState extends State<NewUserPage> {
           );
           return;
         } else {
-          UserBookModel user = UserBookModel(
+          final user = UserBookModel(
             rol: "docente",
             name: _name.text,
             lastName: _lastName.text,
@@ -190,18 +214,93 @@ class _NewUserPageState extends State<NewUserPage> {
                     _email, 'Email', Icons.email, NewUserPage.customColor),
                 const SizedBox(height: 16),
                 if (args.userType == UserType.alumno) ...[
-                  _buildTextField(
-                      _year, 'anio', Icons.person, NewUserPage.customColor,
-                      isNumeric: true),
+                  SizedBox(
+                    width: 800,
+                    child: DropDownTextField(
+                      borderColor: NewUserPage.customColor,
+                      textEditingController: _year,
+                      isSearchVisible: false,
+                      title: 'Año',
+                      hint: 'Selecciona el año',
+                      options: _listYears,
+                      selectedOptions: _selectedYear,
+                      multiple: false,
+                      decoration: InputDecoration(
+                        labelText: 'Año',
+                        prefixIcon: Icon(Icons.title_outlined,
+                            color: NewUserPage.customColor),
+                        suffixIcon: Icon(Icons.arrow_drop_down,
+                            color:
+                                NewUserPage.customColor), // Ícono a la derecha
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: NewUserPage.customColor,
+                            width:
+                                2.0, // Color del borde cuando no está enfocado
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        focusedBorder: OutlineInputBorder(
+                          borderSide: BorderSide(
+                            color: NewUserPage.customColor,
+                            width: 2.0, // Color del borde cuando está enfocado
+                          ),
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                      ),
+                    ),
+                  ),
                   const SizedBox(height: 16),
                   _buildTextField(
                       _div, 'divicion', Icons.school, NewUserPage.customColor,
                       isNumeric: true),
                   const SizedBox(height: 16),
-                  _buildTextField(_career, 'especialidad', Icons.star,
-                      NewUserPage.customColor),
-                  const SizedBox(height: 16),
+                  if (_year.text != _listYears[1] &&
+                      _year.text != _listYears[2] &&
+                      _year.text != _listYears[3])
+                    SizedBox(
+                      width: 800,
+                      child: DropDownTextField(
+                        borderColor: NewUserPage.customColor,
+                        textEditingController: _career,
+                        isSearchVisible: false,
+                        title: 'Especialidad',
+                        hint: 'Selecciona la especialidad',
+                        options: _listCareer,
+                        selectedOptions: _selectedCareer,
+                        multiple: false,
+                        decoration: InputDecoration(
+                          labelText: 'Especialidad',
+                          prefixIcon: Icon(Icons.title_outlined,
+                              color: NewUserPage.customColor),
+                          suffixIcon: Icon(Icons.arrow_drop_down,
+                              color: NewUserPage
+                                  .customColor), // Ícono a la derecha
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: NewUserPage.customColor,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderSide: BorderSide(
+                              color: NewUserPage.customColor,
+                              width: 2.0,
+                            ),
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        ),
+                      ),
+                    ),
                 ],
+                const SizedBox(height: 32),
                 SizedBox(
                   width: 300,
                   child: ElevatedButton(
@@ -222,7 +321,6 @@ class _NewUserPageState extends State<NewUserPage> {
                     ),
                   ),
                 ),
-                const SizedBox(height: 16),
               ],
             ),
           ),
@@ -244,36 +342,42 @@ class _NewUserPageState extends State<NewUserPage> {
         maxLines: maxLines,
         readOnly: isDateField,
         keyboardType: isNumeric ? TextInputType.number : TextInputType.text,
-        inputFormatters: isNumeric
-            ? [
-                FilteringTextInputFormatter.digitsOnly
-              ] // Solo permitir números si es necesario
-            : null,
+        inputFormatters:
+            isNumeric ? [FilteringTextInputFormatter.digitsOnly] : null,
         decoration: InputDecoration(
           labelText: labelText,
           prefixIcon: Icon(icon, color: color),
-          focusedBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: color, width: 2),
+          border: OutlineInputBorder(
+            borderRadius: BorderRadius.circular(10.0),
           ),
           enabledBorder: OutlineInputBorder(
-            borderRadius: BorderRadius.circular(12),
-            borderSide: BorderSide(color: color.withOpacity(0.5), width: 2),
+            borderSide: BorderSide(
+                color: color,
+                width: 2.0), // Color del borde cuando no está enfocado
+            borderRadius: BorderRadius.circular(10.0),
+          ),
+          // Borde cuando el TextField está enfocado
+          focusedBorder: OutlineInputBorder(
+            borderSide: BorderSide(
+                color: color,
+                width: 2.0), // Color del borde cuando está enfocado
+            borderRadius: BorderRadius.circular(10.0),
           ),
         ),
+        onTap: () async {
+          if (isDateField) {
+            DateTime? pickedDate = await showDatePicker(
+              context: context!,
+              initialDate: DateTime.now(),
+              firstDate: DateTime(1900),
+              lastDate: DateTime.now(),
+            );
+            if (pickedDate != null) {
+              controller.text = pickedDate.toIso8601String().split("T")[0];
+            }
+          }
+        },
       ),
     );
-  }
-
-  @override
-  void dispose() {
-    _name.dispose();
-    _lastName.dispose();
-    _dni.dispose();
-    _phone.dispose();
-    _div.dispose();
-    _email.dispose();
-    _year.dispose();
-    super.dispose();
   }
 }
